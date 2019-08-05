a1f4db2d5b437cc6e493a180b60ba5f8cb895404
duarte28nm <53446603+duarte28nm@users.noreply.github.com>
2019-08-05 15:10:23 -0300

    Update Principal.java

--- POO_taveira/aula02/testealuno/encapsulamento/Principal.java
+++ POO_taveira/aula02/testealuno/encapsulamento/Principal.java
@@ -1,64 +1,41 @@
 public class Principal {
 
 	public static void main(String[] args) {
-	
-		
-		//Mousse
-		Mousse mousse1;
-		mousse1 = new Mousse();
-		mousse1.setSabor("Limão");
-		mousse1.setTamanho("Medio");
-		mousse1.setCliente("Kelvin");
-		
-		Mousse mousse2;
-		mousse2 = new Mousse();
-		mousse2.setSabor("Maracuja");
-		mousse2.setTamanho("Pequeno");
-		mousse2.setCliente("Livia");
-		
-		//Pudim
-		
-		Pudim pudim1;
-		pudim1 = new Pudim();
-		pudim1.setSabor("Aoleite");
-		pudim1.setTamanho("Grande");
-		pudim1.setCliente("Neto");
-		
-		Pudim pudim2;
-		pudim2 = new Pudim();
-		pudim2.setSabor("Maracuja");
-		pudim2.setTamanho("Pequeno");
-		pudim2.setCliente("Beatriz");
-		
-		//Mousse
-		System.out.println();
-		System.out.println("Mousse 1");
-		System.out.println("Sabor: "+mousse1.getSabor()); 
-		System.out.println("Tamanho: "+mousse1.getTamanho());
-		System.out.println("Nome do cliente: "+mousse1.getCliente());
-		System.out.println("Muito obrigado, volte sempre ;)");
-		
-		System.out.println();
-		System.out.println("Mousse 2");
-		System.out.println("Sabor: "+mousse2.getSabor()); 
-		System.out.println("Tamanho: "+mousse2.getTamanho());
-		System.out.println("Nome do cliente: "+mousse2.getCliente());
-		System.out.println("Muito obrigado, volte sempre ;)");
-		//Pudim
-		System.out.println();
-		System.out.println("Pudim 1");
-		System.out.println("Sabor: "+pudim1.getSabor()); 
-		System.out.println("Tamanho: "+pudim1.getTamanho());
-		System.out.println("Nome do cliente: "+pudim1.getCliente());
-		System.out.println("Muito obrigado, volte sempre ;)");
-		
-		System.out.println();
-		System.out.println("Pudim 2");
-		System.out.println("Sabor: "+pudim2.getSabor()); 
-		System.out.println("Tamanho: "+pudim2.getTamanho());
-		System.out.println("Nome do cliente: "+pudim2.getCliente());
-		System.out.println("Muito obrigado, volte sempre ;)");
-					
+		       
+		        //Aluno
+				Aluno aluno1;
+				aluno1 = new Aluno();
+				aluno1.setNome("Livia");
+				aluno1.setCpf("145.XXX.XXX.XX");
+				aluno1.setSemestre("P6");
+				aluno1.setMatricula("20162011060512");
+				
+				Aluno aluno2;
+				aluno2 = new Aluno();
+				aluno2.setNome("Neto");
+				aluno2.setCpf("541.XXX.XXX.XX");
+				aluno2.setSemestre("P8");
+				aluno2.setMatricula("20162011060895");
+				
+				
+				//Aluno
+				System.out.println();
+				System.out.println("Aluno 1");
+				System.out.println("Nome: "+aluno1.getNome()); 
+				System.out.println("CPF: "+aluno1.getCpf());
+				System.out.println("Semestre: "+aluno1.getSemestre());
+				System.out.println("Matricula: "+aluno1.getMatricula());
+				System.out.println("Muito obrigado, volte sempre ;)");
+				
+				System.out.println();
+				System.out.println("Aluno 2");
+				System.out.println("Nome: "+aluno2.getNome()); 
+				System.out.println("CPF: "+aluno2.getCpf());
+				System.out.println("Semestre: "+aluno2.getSemestre());
+				System.out.println("Matricula: "+aluno2.getMatricula());
+				System.out.println("Muito obrigado, volte sempre ;)");
+				
+						
 
 	}
 
