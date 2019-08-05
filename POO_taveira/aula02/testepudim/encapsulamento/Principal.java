f498a58bd747257b95cfe55320f8b7e71dd9b1c5
duarte28nm <53446603+duarte28nm@users.noreply.github.com>
2019-08-05 14:15:18 -0300

    Update Principal.java

--- POO_taveira/aula02/testepudim/encapsulamento/Principal.java
+++ POO_taveira/aula02/testepudim/encapsulamento/Principal.java
@@ -1,6 +1,22 @@
 public class Principal {
 
 	public static void main(String[] args) {
+	
+		
+		//Mousse
+		Mousse mousse1;
+		mousse1 = new Mousse();
+		mousse1.setSabor("Limão");
+		mousse1.setTamanho("Medio");
+		mousse1.setCliente("Kelvin");
+		
+		Mousse mousse2;
+		mousse2 = new Mousse();
+		mousse2.setSabor("Maracuja");
+		mousse2.setTamanho("Pequeno");
+		mousse2.setCliente("Livia");
+		
+		//Pudim
 		
 		Pudim pudim1;
 		pudim1 = new Pudim();
@@ -14,7 +30,21 @@ public class Principal {
 		pudim2.setTamanho("Pequeno");
 		pudim2.setCliente("Beatriz");
 		
+		//Mousse
+		System.out.println();
+		System.out.println("Mousse 1");
+		System.out.println("Sabor: "+mousse1.getSabor()); 
+		System.out.println("Tamanho: "+mousse1.getTamanho());
+		System.out.println("Nome do cliente: "+mousse1.getCliente());
+		System.out.println("Muito obrigado, volte sempre ;)");
 		
+		System.out.println();
+		System.out.println("Mousse 2");
+		System.out.println("Sabor: "+mousse2.getSabor()); 
+		System.out.println("Tamanho: "+mousse2.getTamanho());
+		System.out.println("Nome do cliente: "+mousse2.getCliente());
+		System.out.println("Muito obrigado, volte sempre ;)");
+		//Pudim
 		System.out.println();
 		System.out.println("Pudim 1");
 		System.out.println("Sabor: "+pudim1.getSabor()); 
@@ -28,8 +58,8 @@ public class Principal {
 		System.out.println("Tamanho: "+pudim2.getTamanho());
 		System.out.println("Nome do cliente: "+pudim2.getCliente());
 		System.out.println("Muito obrigado, volte sempre ;)");
-										 
-		
+					
+
 	}
 
 }
