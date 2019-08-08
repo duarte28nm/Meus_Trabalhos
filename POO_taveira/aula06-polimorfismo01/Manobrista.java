8145258a6cb1b53f90f15866eeef47e254d15e84
duarte28nm <53446603+duarte28nm@users.noreply.github.com>
2019-08-08 15:14:06 -0300

    Create Manobrista.java

---
+++ POO_taveira/aula06-polimorfismo01/Manobrista.java
@@ -0,0 +1,13 @@
+public class Manobrista  {
+
+    public void estaciona (Veiculo v){
+    	
+        v.vira("Direita");
+        v.vira("Esquerda");
+        v.freia();
+        v.para();
+        System.out.println("");
+        
+    }
+
+}
