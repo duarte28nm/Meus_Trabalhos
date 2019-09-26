b4a034a8aa2c7ae1411e8d1a22018b6100c57b0b
duarte28nm <53446603+duarte28nm@users.noreply.github.com>
2019-09-26 15:35:45 -0300

    Create Main.java

---
+++ POO_taveira/Mapa/Main.java
@@ -0,0 +1,12 @@
+
+public class Main {
+ //FP
+	FolhaPagamento f1 = new FolhaPagamento (9, 2019);
+	
+ //CL
+	Colaborador c1 = new Colaborador (100, "Manoel Claudino", "Av. 13 Maio 2081", "88671020", "Benfica", "60020-060", "124543556-89", 4500.00f);
+	
+ //MF
+	MovimentoFolha m1 = new MovimentoFolha ("100", 4.500,00);
+	
+}
