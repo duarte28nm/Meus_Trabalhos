e841d68c84275327e2eaab5357774c1ab1b8fd3c
duarte28nm <53446603+duarte28nm@users.noreply.github.com>
2019-09-28 14:58:34 -0300

    Update MovimentoFolha.java

--- POO_taveira/Mapa/MovimentoFolha.java
+++ POO_taveira/Mapa/MovimentoFolha.java
@@ -1,26 +1,53 @@
 
-public class MovimentoFolha {
+class MovimentoFolha{
+		
+  private String descrição;
+  private float valor;
+  private TipoMovimento tipoMovimento;
+  private Colaborador colaborador;
 
-	public String descrição;
-	public float valor;
-	
-	
-	public MovimentoFolha (String descrição, float valor ) {
-		this.descrição = descrição;
-		this.valor= valor;
-	}
+  
+  
+  //THIS
+  public MovimentoFolha(Colaborador colaborador, String descrição, float valor, String tipoMovimento){
+    //TIPO = P
+	  if(tipoMovimento == "P"){
+		  this.colaborador = colaborador;
+		  	this.tipoMovimento = TipoMovimento.P;
+		  		this.descrição = descrição;
+		  			this.valor = valor;
+    } else
+    //TIPO = D
+      if(tipoMovimento == "D"){
+    	  this.colaborador = colaborador;
+    	  	this.tipoMovimento = TipoMovimento.D;
+    	  		this.descrição = descrição;
+    	  			this.valor = valor;
+    }
+  }
 
-	
-	public String getDescrição() {
+  
+  
+  //GET E SET
+  public String getDescrição() {
 		return descrição;
 	}
-	public void setDescrição (String descrição) {
+	public void setDescrição(String descrição) {
 		this.descrição = descrição;
-		}
+	}
 	public float getValor() {
 		return valor;
-	}
+  }
+	public Colaborador getColaborador(){
+    return colaborador;
+  }
 	public void setValor(float valor) {
 		this.valor = valor;
+	}
+	public TipoMovimento getTipoMovimento() {
+		return tipoMovimento;
+	}
+	public void setTipoMovimento(TipoMovimento tipoMovimento) {
+		this.tipoMovimento = tipoMovimento;
 	}
 }
